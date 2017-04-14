62e39dacb6753b88c3db2f6e5deead8d895fbf41
narnikgamarnikus <narnikgamarnikus@gmail.com>
2017-04-14 18:36:03 +0300

    changed default DEFAULT_FROM_EMAIL in production config to Punycode

--- config/settings/production.py
+++ config/settings/production.py
@@ -142,7 +142,7 @@ COMPRESS_ENABLED = env.bool('COMPRESS_ENABLED', default=True)
 # EMAIL
 # ------------------------------------------------------------------------------
 DEFAULT_FROM_EMAIL = env('DJANGO_DEFAULT_FROM_EMAIL',
-                         default='gglobal <noreply@ремонт-пк-и-ноутбуков.бел>')
+                         default='gglobal <noreply@xn------dddfnxoenlfghchl4bitc.xn--90ais>')
 EMAIL_SUBJECT_PREFIX = env('DJANGO_EMAIL_SUBJECT_PREFIX', default='[gglobal]')
 SERVER_EMAIL = env('DJANGO_SERVER_EMAIL', default=DEFAULT_FROM_EMAIL)
 
