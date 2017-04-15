183c8ea0df2ea663c2ba2ca82542ebf86254df08
narnikgamarnikus <narnikgamarnikus@gmail.com>
2017-04-15 14:11:04 +0300

    changed .envvars file

--- env/lib/python3.5/site-packages/wagtail/wagtaildocs/apps.py
+++
@@ -1,9 +0,0 @@
-from __future__ import absolute_import, unicode_literals
-
-from django.apps import AppConfig
-
-
-class WagtailDocsAppConfig(AppConfig):
-    name = 'wagtail.wagtaildocs'
-    label = 'wagtaildocs'
-    verbose_name = "Wagtail documents"
