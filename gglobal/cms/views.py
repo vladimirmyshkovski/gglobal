6ae67dbee155ac4f65d52e924dd36a62da20ab2f
narnikgamarnikus <narnikgamarnikus@gmail.com>
2017-04-16 21:23:56 +0300

    added CustomLoginForm, CustomSignupForm, СreateClientView

---
+++ gglobal/cms/views.py
@@ -0,0 +1,18 @@
+from django.http import HttpResponseRedirect, HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from gglobal.crm.flows import ClientFlow
+
+@csrf_exempt
+def СreateClientView(request):
+    if request.method == 'POST':
+        #POST goes here . is_ajax is must to capture ajax requests. Beginner's pit.
+        if request.is_ajax():
+            #Always use get on request.POST. Correct way of querying a QueryDict.
+            name = request.POST.get('name')
+            phone = request.POST.get('phone')
+            data = {"name": name , "phone" : phone}
+            ClientFlow.start.run(data=data)
+            #Returning same data back to browser.It is not possible with Normal submit
+            return JsonResponse(data)
+    #Get goes here
+    return render(request,'base.html')
