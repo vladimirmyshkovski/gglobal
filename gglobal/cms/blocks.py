4e961c2f64f0be206e5e21be8e3eda1dcf6eb8b2
narnikgamarnikus <narnikgamarnikus@gmail.com>
2017-04-25 22:00:42 +0300

    trying fixed bug in migrations

--- gglobal/cms/blocks.py
+++ gglobal/cms/blocks.py
@@ -44,7 +44,10 @@ class FeaturesBlock(StructBlock):
     sub = blocks.CharBlock()
     features_item = CardsBlock(blocks.StructBlock([
     ('title', blocks.CharBlock()),
-    ('sub', blocks.CharBlock()),
+    ('sub', blocks.ListBlock(blocks.StructBlock([
+        ('title', blocks.CharBlock(required=False)),
+        ('link', blocks.PageChooserBlock(required=False)),
+    ]))),
     ('icon', blocks.CharBlock())
 ]))
 
