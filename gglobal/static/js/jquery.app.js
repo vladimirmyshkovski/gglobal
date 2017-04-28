364c01fb36b8b7f100ea17d08a8bcf72aecfa7b9
narnikgamarnikus <narnikgamarnikus@gmail.com>
2017-04-28 13:06:10 +0300

    added raven.js and ajax error handling, upgrade jquery from 2.2.4 to 3.2.1, changed cdn loading scripts to local files

--- gglobal/static/js/jquery.app.js
+++ gglobal/static/js/jquery.app.js
@@ -147,3 +147,69 @@ function($) {
     "use strict";
     $.Page.init()
 }(window.jQuery);
+
+
+$(document).ajaxError(function(event, jqXHR, ajaxSettings, thrownError) {
+    Raven.captureMessage(thrownError || jqXHR.statusText, {
+        extra: {
+            type: ajaxSettings.type,
+            url: ajaxSettings.url,
+            data: ajaxSettings.data,
+            status: jqXHR.status,
+            error: thrownError || jqXHR.statusText,
+            response: jqXHR.responseText.substring(0, 100)
+        }
+    });
+});
+
+        function csrfSafeMethod(method) {
+        // these HTTP methods do not require CSRF protection
+        return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+        }
+        $.ajaxSetup({
+            beforeSend: function(xhr, settings) {
+                if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
+                    xhr.setRequestHeader("X-CSRFToken", csrftoken);
+                }
+            }
+        });
+
+
+
+            $.ajaxSetup({ 
+             beforeSend: function(xhr, settings) {
+                 function getCookie(name) {
+                     var cookieValue = null;
+                     if (document.cookie && document.cookie != '') {
+                         var cookies = document.cookie.split(';');
+                         for (var i = 0; i < cookies.length; i++) {
+                             var cookie = jQuery.trim(cookies[i]);
+                             // Does this cookie string begin with the name we want?
+                             if (cookie.substring(0, name.length + 1) == (name + '=')) {
+                                 cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                                 break;
+                             }
+                         }
+                     }
+                     return cookieValue;
+                 }
+                 if (!(/^http:.*/.test(settings.url) || /^https:.*/.test(settings.url))) {
+                     // Only send the token to relative URLs i.e. locally.
+                     xhr.setRequestHeader("X-CSRFToken", getCookie('csrftoken'));
+                 }
+             } 
+        });
+
+        $('ul.dropdown-menu [data-toggle=dropdown]').on('click', function(event) {
+         event.preventDefault();
+         event.stopPropagation();
+         $(this).parent().addClass('open');
+         var menu = $(this).parent().find("ul");
+         var menupos = menu.offset();
+         if ((menupos.left + menu.width()) + 30 > $(window).width()) {
+         var newpos = - menu.width();
+         } else {
+         var newpos = $(this).parent().width();
+         }
+         menu.css({ left:newpos });
+        });
