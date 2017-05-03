fd3d4f677bbf0fef35040cb1c2183abb95f48542
narnikgamarnikus <narnikgamarnikus@gmail.com>
2017-05-03 20:57:00 +0300

    added return self.user.get_full_name() to the def __str__ MasterCRMProfile

--- gglobal/crm/models.py
+++ gglobal/crm/models.py
@@ -45,7 +45,7 @@ class MasterCRMProfile(models.Model):
         verbose_name_plural = "Мастера"
 
     def __str__(self):  # pragma: no cover
-        return self.user.get_full_name()
+        return self.name
 
 
 class ClientProcess(Process):
