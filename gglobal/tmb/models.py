2ced776755fc3e06425c56b964cadfe5bb1cc262
narnikgamarnikus <narnikgamarnikus@gmail.com>
2017-06-23 05:35:53 +0300

    changed cms models

--- gglobal/tmb/models.py
+++ gglobal/tmb/models.py
@@ -32,7 +32,7 @@ class Bot(models.Model):
 		if not self.pk:
 			self.get_me
 			TelegramBot = telepot.Bot(self.api_key)
-			TelegramBot.setWebhook('xn--90a0am.xn------dddfnxoenlfghchl4bitc.xn--90ais/bot/bot/{bot_token}/'.format(bot_token=self.api_key))
+			TelegramBot.setWebhook('xn------dddfnxoenlfghchl4bitc.xn--90ais/bot/bot/{bot_token}/'.format(bot_token=self.api_key))
 		super(Bot, self).save(*args, **kwargs)
 
 	class Meta:
