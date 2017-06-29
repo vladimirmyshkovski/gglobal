08136696c771952eda96ef788369484bacc37046
narnikgamarnikus <narnikgamarnikus@gmail.com>
2017-06-30 00:52:05 +0300

    small changed in crm admin

--- scripts/clear.sh
+++ scripts/clear.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 for i in `find /var/lib/nginx/cache -type f`; do rm $i ; done
-#python manage.py invalidate_cachalot
+python manage.py invalidate_cachalot
---
+++ scripts/run.sh
@@ -0,0 +1,6 @@
+. /var/www/gglobal/venv/bin/activate
+pip install -r requirements/production.txt
+while IFS='' read -r line || [[ -n "$line"  ]]; do
+        export $line
+        echo exporting $line
+done < "$1"
