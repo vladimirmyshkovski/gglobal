5de7440c5e98e52f57be81e1587b3bf2f1a89a95
narnikgamarnikus <narnikgamarnikus@gmail.com>
2017-07-05 01:03:47 +0300

    changed service forms

--- gglobal/service/forms.py
+++ gglobal/service/forms.py
@@ -3,8 +3,8 @@ from gglobal.service.models import Service
 
 
 class ServiceForm(forms.ModelForm):
-    description = forms.CharField(widget=forms.Textarea)
-    
+    description = forms.CharField(widget=forms.Textarea, required=False, label='Описание')
+
     class Meta:
     	model = Service
     	fields = '__all__'
