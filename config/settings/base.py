f5f515cbf05ca8ae081a5139df20eab30f9937b8
narnikgamarnikus <narnikgamarnikus@gmail.com>
2017-07-05 03:05:30 +0300

    changed base settings

--- config/settings/base.py
+++ config/settings/base.py
@@ -194,8 +194,8 @@ INSTALLED_APPS = PRE_DJANGO_APPS + DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
 # MIDDLEWARE CONFIGURATION
 # ------------------------------------------------------------------------------
 MIDDLEWARE = [
-    #'django_hosts.middleware.HostsRequestMiddleware',
-    #'django.middleware.cache.UpdateCacheMiddleware',
+    'django_hosts.middleware.HostsRequestMiddleware',
+    'django.middleware.cache.UpdateCacheMiddleware',
     #'django.middleware.gzip.GZipMiddleware',
     #'htmlmin.middleware.HtmlMinifyMiddleware',
     #'nplusone.ext.django.NPlusOneMiddleware',
@@ -215,8 +215,8 @@ MIDDLEWARE = [
     #'wagtailthemes.middleware.ThemeMiddleware',
     'cuser.middleware.CuserMiddleware',
     'turbolinks.middleware.TurbolinksMiddleware',
-    #'django.middleware.cache.FetchFromCacheMiddleware',
-    #'django_hosts.middleware.HostsResponseMiddleware',
+    'django.middleware.cache.FetchFromCacheMiddleware',
+    'django_hosts.middleware.HostsResponseMiddleware',
     #'htmlmin.middleware.MarkRequestMiddleware',
 ]
 
