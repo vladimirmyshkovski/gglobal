60b682633fd4910ddc962319c0e55fae4b0f5765
narnikgamarnikus <narnikgamarnikus@gmail.com>
2017-07-05 03:29:36 +0300

    changed base settings, urls and hosts settings

--- config/hosts.py
+++ config/hosts.py
@@ -2,6 +2,6 @@ from django_hosts import patterns, host
 from django.conf import settings
 
 host_patterns = patterns('',
-    host(r'(\w+)', 'gglobal.city.urls', name='wildcard'),
 	host(r'', settings.ROOT_URLCONF, name='default'),
+    host(r'(?!www)\w+', 'gglobal.city.urls', name='wildcard'),
 )
