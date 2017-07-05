fde32871e61847777c4770214c4529613c2c3496
narnikgamarnikus <narnikgamarnikus@gmail.com>
2017-07-05 06:27:29 +0300

    changed hosts

--- config/hosts.py
+++ config/hosts.py
@@ -1,8 +1,9 @@
 from django_hosts import patterns, host
 from django.conf import settings
+from gglobal.city import urls
 
 host_patterns = patterns('',
 	host(r'', settings.ROOT_URLCONF, name='default'),
-    host(r'xn--c1ac1acci', 'gglobal.city.urls', name='cities'),
+    host(r'xn--c1ac1acci', urls, name='cities'),
 )
 
