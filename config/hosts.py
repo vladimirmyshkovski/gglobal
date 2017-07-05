7db95d6df5b2f5f01a4f909fe000e3d65f1f0706
narnikgamarnikus <narnikgamarnikus@gmail.com>
2017-07-05 10:21:03 +0300

    fixed bug with crm urls

--- config/hosts.py
+++ config/hosts.py
@@ -3,6 +3,6 @@ from django.conf import settings
 
 host_patterns = patterns('',
 	host(r'', settings.ROOT_URLCONF, name='default'),
-    host(r'(?P<alternate_names>[\w]+)', 'gglobal.city.urls', name='cities'),
+    host(r'(?P<alternate_names>[\w+])', 'gglobal.city.urls', name='cities'),
 )
 
