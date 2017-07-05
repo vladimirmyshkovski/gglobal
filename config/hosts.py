7dfc1b9fc4600d744171644c1e4eb9f1dea57dd0
narnikgamarnikus <narnikgamarnikus@gmail.com>
2017-07-05 03:50:25 +0300

    changed cities views and urls

--- config/hosts.py
+++ config/hosts.py
@@ -3,5 +3,6 @@ from django.conf import settings
 
 host_patterns = patterns('',
 	host(r'', settings.ROOT_URLCONF, name='default'),
-    host(r'(?!www)\w+', 'gglobal.city.urls', name='wildcard'),
+    host(r'(\w+)', 'gglobal.city.urls', name='wildcard'),
 )
+
