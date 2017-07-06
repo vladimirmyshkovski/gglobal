b5ea947ccea46b893cd169b018601c9de3044927
narnikgamarnikus <narnikgamarnikus@gmail.com>
2017-07-06 10:42:23 +0300

    added def save() to Service model

--- gglobal/service/models.py
+++ gglobal/service/models.py
@@ -30,12 +30,12 @@ class Service(MPTTModel):
     class MPTTMeta:
         #level_attr = 'name'
         order_insertion_by = ['name']
-    '''
+    
     def save(self, *args, **kwargs):
         if not self.id or not self.slug:
             self.slug = slugify(self.name)
         super(Service, self).save(*args, **kwargs)
-    '''
+    
     def __str__(self):
     	return '%s' % self.name
 
