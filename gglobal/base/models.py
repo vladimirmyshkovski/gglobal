1e731a3171259dcb436634ad611aab5dd21df64d
narnikgamarnikus <narnikgamarnikus@gmail.com>
2017-07-07 18:11:14 +0300

    add free cropping to Base Image

--- gglobal/base/models.py
+++ gglobal/base/models.py
@@ -10,7 +10,7 @@ class Image(models.Model):
 	content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
 	object_id = models.PositiveIntegerField()
 	content_object = GenericForeignKey('content_type', 'object_id')
-	cropping = ImageRatioField('image', '1200x960')
+	cropping = ImageRatioField('image', '1200x960', free_crop=True)
 	#tags = TaggableManager()
 
 	class Meta:
