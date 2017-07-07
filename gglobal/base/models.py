85e4b72beda09fb0af7a94aee0e7c189f5b6e507
narnikgamarnikus <narnikgamarnikus@gmail.com>
2017-07-07 16:32:27 +0300

    changed cropping size on Base image

--- gglobal/base/models.py
+++ gglobal/base/models.py
@@ -10,7 +10,7 @@ class Image(models.Model):
 	content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
 	object_id = models.PositiveIntegerField()
 	content_object = GenericForeignKey('content_type', 'object_id')
-	cropping = ImageRatioField('image', '600x400')
+	cropping = ImageRatioField('image', '800x600')
 	#tags = TaggableManager()
 
 	class Meta:
